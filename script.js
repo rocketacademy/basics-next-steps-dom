694ba759658e7cbc7afb00340d4ac74219a7d893
consxious <tengkimyen@gmail.com>
2021-12-16 00:21:24 +0800

    exercise done

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('yay!');
+// console.log('yay!');
+
+// make the variables out (from html id) of the input & button
+var input = document.querySelector("#starter-ex");
+var button = document.querySelector("#starter-button");
+
+// call this function
+var myButtonClicked = function () {
+  // assign value typed from input to the variable
+  var typedValue = input.value;
+  // create a new h2 element
+  var newHTwo = document.createElement("h2");
+  // set text inside newHTwo element
+  newHTwo.innerText = typedValue;
+  // make newHTwo appear on screen
+  document.body.appendChild(newHTwo);
+  // Empty the HTML input box
+  input.value = "";
+};
+// Activate the function when user *click*
+button.addEventListener("click", myButtonClicked);
